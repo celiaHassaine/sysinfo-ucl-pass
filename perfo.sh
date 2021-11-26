a51e15b5bd34043a9bc0130cf69406d0097e8189
celiaHassaine <44391501+celiaHassaine@users.noreply.github.com>
2021-11-26 17:09:13 +0100

    Add files via upload

--- perfo.sh
+++ perfo.sh
@@ -2,25 +2,48 @@
 
 make clean -s 2>1&>/dev/null
 make clean_csv -s 2>1&>/dev/null
-make prod
+make prod -s 2>1&>/dev/null
+
+prog_names=("prod" "philo" "reader")
 
 nbr_coeur=4
 
-val= 1
+moyenne_philo=0
+moyenne_reader=0
 
-for ((i=0; i<3; i++))
-do
-	echo "coeur, secondes écoulées 1, secondes écoulées 2, secondes écoulées 3, secondes écoulées 4, secondes écoulées 5" >> mesures_${programs[$i]}.csv
-    for ((i=0; i<$nbr_coeur*2; i++))
+val=1
+
+#for ((i=0; i<3; i++))
+#do
+	echo "coeur, secondes écoulées 1, secondes écoulées 2, secondes écoulées 3, secondes écoulées 4, secondes écoulées 5, moyenne" >> mesures_${prog_names[$i]}.csv
+    for ((t=0; t<$nbr_coeur*2; t++))
     do
-        #on incrémente val que si t est impair
-            if (($t % 2 != 0 ))
-            then
-                val=$((val + 1 ))
-            fi
-            temps=$(/usr/bin/time -f "%e" ./prod $val $val 2>&1)
-            echo  $t,$temps >> mesures_${programs[$i]}.csv
-    fi
+        #if (${prog_names[$i]} == "prod") 
+        #then
+            string_fin="$t"
+            moyenne_prod=0
+            for ((repeat=0; repeat<5; repeat++))
+            do
+                temps=$(/usr/bin/time -f "%e" ./prod $val $val 2>&1|tail -n 1)
+                moyenne_prod="$(echo "$moyenne_prod+$temps" | bc)$(echo "$moyenne_prod+$temps" | bc -l)"
+                echo $moyenne_prod
+                string_fin="$string_fin,$temps"
+            done
+            moyenne_prod=$(echo "$moyenne_prod/5" | bc -l);
+            moyenne_prod=$(printf ",%.2f" $moyenne_prod)
+            string_fin="$string_fin,$moyenne_prod"
+            echo $string_fin
+            echo $string_fin >> mesures_${prog_names[$i]}.csv
+        #fi
+        #if (${prog_names[$i]} == "prod") 
+        #then
+        #    temps=$(/usr/bin/time -f "%e" ./prod $val $val 2>&1)
+        #fi
+        #if (${prog_names[$i]} == "prod") 
+        #then
+        #    temps=$(/usr/bin/time -f "%e" ./prod $val $val 2>&1)
+        #fi
+    done
 
-fi
+#done
 
