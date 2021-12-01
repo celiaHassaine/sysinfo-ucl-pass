97912a2b6984949dfa16db164f3cb08b35608e71
celia <celia_hassaine@hotmail.com>
2021-12-01 09:00:05 +0100

    3 diff graph for each algo

--- graphs/graph.py
+++ graphs/graph.py
@@ -6,28 +6,67 @@ import pandas as pd
 import numpy as np
 import os
 
-prog_names=["prod", "philo", "reader"]
+def get_std(array):
+    arr = [0] * 8
+    i=0
 
-i = 0
-data_prod = pd.read_csv('mesures_prod.csv')
-data_philo = pd.read_csv('mesures_philo.csv')
-data_reader = pd.read_csv('mesures_reader.csv')
+    while i<8:
+        arr[i]=np.std(array.T[i])
+        i=i+1
+
+    return arr
+
+files_csv=["mesures_prod.csv", "mesures_philo.csv", "mesures_reader.csv"]
+
+data_prod = pd.read_csv(files_csv[0])
+data_philo = pd.read_csv(files_csv[1])
+data_reader = pd.read_csv(files_csv[2])
+
+ecart_prod=pd.read_csv(files_csv[0], usecols=['sec_1', 'sec_2', 'sec_3', 'sec_4', 'sec_5'])
+ecart_philo=pd.read_csv(files_csv[1], usecols=['sec_1', 'sec_2', 'sec_3', 'sec_4', 'sec_5'])
+ecart_reader=pd.read_csv(files_csv[2], usecols=['sec_1', 'sec_2', 'sec_3', 'sec_4', 'sec_5'])
+
+
 plt.figure(figsize=(14.8, 12.2))
 plt.title('measuresements of time through 3 different algorithm')
 
 x = range(len(data_prod['coeur']))
 plt.plot(x, data_prod['moyenne'])
+plt.errorbar(x, data_prod['moyenne'], get_std(ecart_prod), capsize=10, fmt=' ', ecolor='green')
 plt.xticks(x, data_prod['coeur'])
+plt.xlabel('number of threads')
+plt.ylabel('average time to execute')
+plt.ylim(ymin=0)
+plt.legend(["prod-cons moyenne", "prod-cons ecart-type"])
+plt.savefig('graphs/graph_prod.png')
+
+plt.figure().clear()
+
+plt.figure(figsize=(14.8, 12.2))
+plt.title('measuresements of time through 3 different algorithm')
 
 x = range(len(data_philo['coeur']))
 plt.plot(x, data_philo['moyenne'])
+plt.errorbar(x, data_philo['moyenne'], get_std(ecart_philo), capsize=10, fmt=' ', ecolor='red')
 plt.xticks(x, data_philo['coeur'])
+plt.xlabel('number of threads')
+plt.ylabel('average time to execute')
+plt.ylim(ymin=0)
+plt.legend(["philo moyenne", "philo ecart-type"])
+plt.savefig('graphs/graph_philo.png')
+
+plt.figure().clear()
+
+plt.figure(figsize=(14.8, 12.2))
+plt.title('measuresements of time through 3 different algorithm')
 
 x = range(len(data_reader['coeur']))
 plt.plot(x, data_reader['moyenne'])
+plt.errorbar(x, data_reader['moyenne'], get_std(ecart_reader), capsize=10, fmt=' ', ecolor='blue')
 plt.xticks(x, data_reader['coeur'])
 
 plt.xlabel('number of threads')
 plt.ylabel('average time to execute')
-plt.legend(["prod-cons algo", "philo algo", "reader algo"])
-plt.show()
+plt.ylim(ymin=0)
+plt.legend(["reader moyenne", "reader ecart-type"])
+plt.savefig('graphs/graph_reader.png')
