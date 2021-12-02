d966fcb83f8098c8706bf6ec1b993c7df091be8a
andebelle <62747585+andebelle@users.noreply.github.com>
2021-12-02 16:36:45 +0100

    Add files via upload
    Normalement c'est okay

---
+++ test-and-set.c
@@ -0,0 +1,59 @@
+//Test-and-set
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <errno.h>
+#include <string.h>
+
+int l= 0; //lock ==0 
+int N; //Nombre de threads
+
+//Fonction lock
+void lock(void){
+    int t=1;
+    while(t == 1){
+        asm volatile ("movl %1, %%eax;"
+        "xchgl %%eax, %0;"
+        :"=m" (l)
+        :"m" (t)
+        :"%eax"
+        );
+    }
+}
+//Fonction unlock
+void unlock(void){
+    int t=0;
+    asm ("movl %0, %%eax;"   
+        "xchgl %%eax, %1;"
+        :"=m" (l)
+        :"m" (t)
+        :"%eax"
+    );
+}
+
+//Fonction pour testterle lock et unlock
+void section_critique(void){
+	
+	int count = 0;
+	
+	while(count < 6400/N){
+	    lock();
+	
+	    while(rand() > RAND_MAX/10000){}
+	        unlock();
+	    count ++;}
+	}
+
+int main(int argc, char *argv[]){
+	N = atoi(argv[1]);
+	pthread_t threads[N]; //ON crée la tableeau de threads
+	for(int i = 0; i<N; i++){
+		pthread_create(&threads[i],NULL,(void *) section_critique,NULL);
+		}
+	for(int i = 0; i<N; i++){
+		pthread_join(threads[i],NULL);
+		}
+	}
