1b9d2d9282233325a066bfcb8a5e860ce69cdb93
andebelle <62747585+andebelle@users.noreply.github.com>
2021-12-05 17:09:27 +0100

    Update test-and-test-and-set.c

--- part_2/test-and-test-and-set.c
+++ part_2/test-and-test-and-set.c
@@ -18,8 +18,8 @@ void init_lock(int volatile *verrou){
 //Fonction lock
 void lock(int volatile *verrou){
     int t=1;
-    while(t == 1){
-        while(l==1){};
+    while(t == 1)
+        while(verrou==1){};
         asm volatile ("movl $1, %%eax;"
         "xchgl %%eax, %1;"
         "movl %%eax, %0"
@@ -43,7 +43,7 @@ void unlock(int volatile *verrou){
 int volatile verrou;
 
 //Fonction pour testterle lock et unlock
-void test_and_set(void){
+void test_and_test_and_set(void){
 	
 	int count = 0;
 	
@@ -59,7 +59,7 @@ int main(int argc, char *argv[]){
 	N = atoi(argv[1]);
 	pthread_t threads[N]; //ON crée la tableeau de threads
 	for(int i = 0; i<N; i++){
-		pthread_create(&threads[i],NULL,(void *) test_and_set,NULL);
+		pthread_create(&threads[i],NULL,(void *) test_and_test_and_set,NULL);
 		}
 	for(int i = 0; i<N; i++){
 		pthread_join(threads[i],NULL);
