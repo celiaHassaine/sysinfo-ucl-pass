e41843a9a0206eccf6c58186169fb84db17f8b52
celia <celia_hassaine@hotmail.com>
2021-12-06 21:51:39 +0100

    semaphore done

---
+++ part_2/algo_tas/semaphore_tas.c
@@ -0,0 +1,121 @@
+//Interface sémaphore
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <errno.h>
+#include <string.h>
+#include "semaphore_tas.h"
+
+// Functions from test and test and set.c
+
+sem *sema;
+
+
+void init_lock(int volatile *verrou){
+    *verrou = 0;
+}
+
+//Fonction lock
+void lock(int volatile *verrou){
+    int t=1;
+    printf("hello\n");
+    while(t == 1){
+        printf("t = %d\n", *verrou);
+        asm volatile ("movl $1, %%eax;"
+        "xchgl %%eax, %1;"
+        "movl %%eax, %0"
+        :"=m" (t)
+        :"m" (verrou)
+        :"%eax"
+        );
+    }
+}
+//Fonction unlock
+void unlock(int volatile *verrou){
+    int t=0;
+    asm ("movl $0, %%eax;"   
+        "xchgl %%eax, %1;"
+        :"=m" (verrou)
+        :"m" (t)
+        :"%eax"
+    );
+}
+
+
+
+int semaphore_init(sem *s, int v){
+    s->val=v;
+    init_lock(s->verrou);
+    return 0;
+}
+
+
+void semaphore_wait(sem *s){
+    int volatile *test = s->verrou;
+    printf("%d\t%d\n", *(s->verrou), s->val);
+    while(s->val <= 0);
+    //printf("%ls\n", test);
+	lock(test);
+    printf("%d\n", s->val);
+	s->val--;
+	unlock(test);
+}
+
+
+void semaphore_post(sem *s){
+    int volatile *test = s->verrou;
+    lock(test);
+    s->val = s->val+1; //Incrémente la valeur du sémaphore
+    unlock(test);
+}
+
+
+
+void semaphore_destroy(sem *s){
+    /*int volatile *test = s->verrou;
+    free(&test);*/
+    free(s);
+}
+/*
+
+int N; //Nombre de threads
+
+void test_and_set_sem(void){
+	
+	int count = 0;
+	
+	while(count < 6400/N){
+        //printf("début sem value: %d\n", sema->val);
+		semaphore_post(sema);
+        //printf("1 post sem value: %d\n", sema->val);
+		semaphore_wait(sema);
+        //printf("1 post + 1 wait sem value: %d\n", sema->val);
+		while(rand() > RAND_MAX/10000);
+	    count ++;
+		semaphore_post(sema);
+        //printf("1 post + 1 wait + 1 post sem value: %d\n", sema->val);
+		semaphore_wait(sema);
+        //printf("1 post + 1 wait + 1 post + 1 wait sem value: %d\n", sema->val);
+    }
+}
+
+void main(int argc, char *argv[]){
+    printf("HERE\n");
+	N = atoi(argv[1]);
+	pthread_t threads[N]; //ON crée la tableeau de threads
+    printf("trying malloc\n");
+    sema=malloc(sizeof(sem));
+    (*sema).verrou = malloc(sizeof(int volatile));
+    semaphore_init(sema, 0);
+    printf("%d\n%d\n", sema->val, *(sema->verrou));
+	for(int i = 0; i<N; i++){
+		pthread_create(&threads[i],NULL,(void *) test_and_set_sem,NULL);
+		}
+	for(int i = 0; i<N; i++){
+		pthread_join(threads[i],NULL);
+	}
+    semaphore_destroy(sema);
+}*/
---
+++ part_2/algo_ttas/perfo_ttas.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+make clean -s 2>1&>/dev/null
+make clean_csv -s 2>1&>/dev/null
+make prod_ttas -s 2>1&>/dev/null
+make reader_ttas -s 2>1&>/dev/null
+make philo_ttas -s 2>1&>/dev/null
+
+prog_names=("prod_ttas" "philo_ttas" "reader_ttas")
+
+nbr_coeur=4
+prod_writer=0
+cons_reader=0
+
+for ((i=0; i<3; i++))
+do
+	echo "thread,sec_1,sec_2,sec_3,sec_4,sec_5,moyenne" >> mesures_${prog_names[$i]}.csv
+    for ((t=1; t<($nbr_coeur*2)+1; t++))
+    do
+        if (($t%2 == 0))
+        then
+            prod_writer=$(echo "$t/2" | bc)
+            cons_reader=$(echo "$t/2" | bc)
+        elif (($t==1))
+        then
+            prod_writer=$t
+            cons_reader=$t
+        else
+            prod_writer=$(echo "($t-1)/2" | bc)
+            cons_reader=$(echo "($t+1)/2" | bc)
+        fi
+        string_fin="$t"
+        moyenne=0
+        if ((${prog_names[$i]} == "prod"))
+        then
+            for ((repeat=0; repeat<5; repeat++))
+            do
+                temps=$(/usr/bin/time -f "%e" ./prod_ttas $prod_writer $cons_reader 2>&1|tail -n 1)
+                moyenne=$(echo "$moyenne+$temps" | bc -l)
+                string_fin="$string_fin,$temps"
+            done
+        elif ((${prog_names[$i]} == "reader"))
+        then
+            for ((repeat=0; repeat<5; repeat++))
+            do
+                temps=$(/usr/bin/time -f "%e" ./reader_ttas $prod_writer $cons_reader 2>&1|tail -n 1)
+                moyenne=$(echo "$moyenne+$temps" | bc -l)
+                string_fin="$string_fin,$temps"
+            done
+        elif ((${prog_names[$i]} == "philo"))
+        then
+            for ((repeat=0; repeat<5; repeat++))
+            do
+                temps=$(/usr/bin/time -f "%e" ./philo_ttas $t 2>&1|tail -n 1)
+                moyenne=$(echo "$moyenne+$temps" | bc -l)
+                string_fin="$string_fin,$temps"
+            done
+        fi
+        #getting the average time out of 5
+        moyenne=$(echo "$moyenne/5" | bc -l);
+        moyenne=","${moyenne:1}
+        moyenne="0$moyenne"
+        moyenne=$(printf "%.3f" $moyenne)
+        moyenne=${moyenne//[,]/.}
+
+        string_fin="$string_fin,$moyenne"
+        echo $string_fin >> mesures_${prog_names[$i]}.csv
+    done
+
+done
+
